2a6172ddd668fbd5021242726223bf1558e165f0
LE2HE <leehe4352@gamil.com>
2022-06-03 09:23:57 +0900

    Implement #12 - Reponse
    Response 내려주는 방법 구현
    (TEXT/JSON/ResponseEntity/ResponseBody)

---
+++ src/main/java/com/example/hello/controller/PageController.java
@@ -0,0 +1,28 @@
+package com.example.hello.controller;
+
+import com.example.hello.dto.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/main")
+    public String main() {
+        return "main.html";
+    }
+
+    //ResponseEntity
+
+    //ResponseBody
+    @ResponseBody
+    @GetMapping("/user")
+    public User user() {
+        var user = new User();
+        user.setName("steve");
+        user.setAddress("패스트캠퍼스");
+        return user;
+    }
+}
